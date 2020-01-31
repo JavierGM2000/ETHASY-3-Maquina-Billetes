ad23a6f2afd054e0aaed8d8ea4058c13eb4840ea
Urki <urki66@gmail.com>
2020-01-31 15:21:22 +0100

    Ventana Seleccionar Operación creada

--- Ethasy3MaquinaBilletes/src/ethasy3maquinabilletes/Main.java
+++ Ethasy3MaquinaBilletes/src/ethasy3maquinabilletes/Main.java
@@ -1,53 +1,54 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package ethasy3maquinabilletes;
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.lang.Object;
-import javax.swing.*;
-import java.awt.event.*;
-/**
- *
- * @author Suplente
- */
- public class Main extends JFrame implements ActionListener{
 
-    static public class VentanaPrincipal extends JFrame
-    {
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-       private JButton consultar,comprar;
-       
-       public Main(){
 
-           consultar=new JButton();
-           consultar.setBounds(30,50,170,170);
-           add(consultar);
-           consultar.addActionListener(this);
-
-           comprar=new JButton();
-           comprar.setBounds(200,50,170,170);
-           add(comprar);
-           comprar.addActionListener(this);
+public class Main extends JFrame{
+   
+     private JButton consultar,comprar,logout;
+     private JLabel seleccioneOperacion;
+             
+     public Main(){
+             
+          setLayout(null);
          
-
+          //label
+           seleccioneOperacion= new JLabel("Seleccione la operación que desee hacer:");
+           seleccioneOperacion.setBounds(90,-10,650,250);
+           seleccioneOperacion.setFont (seleccioneOperacion.getFont ().deriveFont (30f));
+           add(seleccioneOperacion);
+           
+          //Consultar
+           consultar=new JButton("Consultar tus tickets");
+           consultar.setBounds(90,170,250,250);
+           consultar.setFont (consultar.getFont ().deriveFont (20f));
+           add(consultar);
+           
+          //Comprar
+           comprar=new JButton("Comprar ticket");
+           comprar.setBounds(440,170,250,250);
+           comprar.setFont (comprar.getFont ().deriveFont (20f));
+           add(comprar);
+           
+           //logout
+           logout=new JButton("Logout");
+           logout.setBounds(550,20,120,50);
+           add(logout);
+           
+           
+    }
+     
+     public static void main(String[] ar){
+           
+            Main seleccion=new Main();
+            seleccion.setBounds(0, 0, 800, 600);
+            seleccion.setVisible(true);
+            seleccion.setTitle("Selección de operación");
         }
 
    
-
-   
-    public static void main(String[] args) {
-        // TODO code application logic here
-        /*MEGATEST*/
-        VentanaPrincipal Princip= new VentanaPrincipal();
-        Princip.setBounds(0,0,800,600);
-        Princip.setVisible(true);
-        Princip.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Princip.setResizable(false);
-    }
-
-}
 }
